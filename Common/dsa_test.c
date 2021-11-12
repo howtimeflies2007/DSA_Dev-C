c86b6df6b77d0b9989910e202e9eeefb2b194d23
howtimeflies2007 <wang_zida@126.com>
2021-11-12 13:41:23 +0800

    Architecture for data structure test
    Initial test code for data structure.

---
+++ Common/dsa_test.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef void (*func)(void);
+
+typedef struct {
+	char testName[64];
+	func testFunc;
+} testSuit;
+
+extern void LRU_test();
+extern void addTwoNumber_test();
+
+testSuit test[] = {
+	{ "LRU", 				LRU_test },
+	{ "addTwoNum", 			addTwoNumber_test },
+};
+
+void testRun(char *testName)
+{
+	int testCount = sizeof(test) / sizeof(testSuit);
+	
+	int i = 0;
+	for (; i < testCount; i++)
+	{
+		if (!strncmp(testName, test[i].testName, strlen(testName)))
+		{
+			printf("Ready to run: %s\n", test[i].testName);
+			test[i].testFunc();
+			break;
+		}
+	}
+	
+	if (i == testCount)
+	{
+		printf("No test function found!\n");
+	}
+}
